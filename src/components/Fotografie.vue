1b978afeb9c3f01f5714561d79b3168c085d0ff1
Phelipe de Sterlich <p.desterlich@gmail.com>
2017-11-21 11:15:34 +0100

    aggiunto elenco fotografie

---
+++ src/components/Fotografie.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <h1>elenco fotografie</h1>
+  <ul>
+    <li v-for="item in elenco">{{ item.fileName }}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Fotografie',
+    data () {
+      return {
+        elenco: []
+      }
+    },
+    methods: {
+      getList () {
+        this.$http.get('api/fotografie')
+          .then(response => {
+            console.log('ok')
+            this.elenco = response.body
+          }, response => {
+            console.log('error')
+            console.log(response)
+          })
+      }
+    },
+    created () {
+      this.getList()
+    }
+  }
+</script>
+
+<style>
+
+</style>
